b21392a9ea4a659a021c7bbbb04b9c353c704a61
Edson Alves Moisinho <edson.alves.moisinho@avanade.com>
2018-07-12 10:24:32 -0300

    alterando a porta

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import * as restify from 'restify';
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8081
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 function respond(req, res, next) {
-  res.send('hello 6 ' + req.params.name);
+  res.send('hello ' + req.params.name);
   next();
 }
 
